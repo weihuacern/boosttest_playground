ab32c34629083a750974219e2c43823078e429df
weihuacern <hua.wei@email.ucr.edu>
2020-03-11 16:59:59 -0400

    more solutions

--- src/solution.hpp
+++ src/solution.hpp
@@ -1,4 +1,5 @@
-#include<vector>
+#include <string>
+#include <vector>
 
 class Solution {
     public:
@@ -7,5 +8,12 @@ class Solution {
     // Destructor
     ~Solution();
 
+    std::vector<std::string> AlmostEquivalentStrings(std::vector<std::string>& s, std::vector<std::string>& t);
     int IdenticalDistribution(int n, std::vector<int>& snacks);
+    int AutoscalePolicy(int ins, int n, std::vector<int>& ticks);
+    int FactorOf3And5(int l, int r);
+    std::vector<int> AscendingBinarySorting(std::vector<int>& nums);
+    int ConnectingComputers(int nNodes, int nEdges, std::vector<int>& nodeF, std::vector<int>& nodeT);
+    int CircularArray(int n, int m, std::vector<int>& endNode);
+    int BucketFill(std::vector<std::string>& picture);
 };
